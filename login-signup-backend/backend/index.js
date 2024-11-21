8568847c462369d6ed4d779029cd4e9f9a8f1d44
Yogesh Rathod <yogeshrathod68900@gmail.com>
2024-11-21 13:34:26 +0530

    "authorization and authentication backend setup ready"
    server is ready to listen

---
+++ login-signup-backend/backend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("server is ready...");
+});
